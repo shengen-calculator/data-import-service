e1dfc2d55e2d5d75e21be0c925a58d39ff008417
Bass <suddya@toptal.com>
2021-05-23 11:41:04 +0300

    remove unused using

--- DataImport.Infrastructure.Services/IEmailService.cs
+++ DataImport.Infrastructure.Services/IEmailService.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using DataImport.Core.Domain;
-
 namespace DataImport.Infrastructure.Services
 {
     public interface IEmailService
